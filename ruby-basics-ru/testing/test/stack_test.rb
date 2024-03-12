46e4fc103b158880118457a5c27ad553d96b85f1
@hexlet/cli <support@hexlet.io>
2024-03-12 23:08:37 +0300

    submit ruby-basics-ru/testing

---
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+require_relative '../lib/stack'
+require 'minitest/power_assert'
+
+class StackTest < Minitest::Test
+  # BEGIN
+  def setup
+    @data = Stack.new(%w[php js ruby])
+  end
+
+  def test_push!
+    elem = 'python'
+
+    expected = @data.size + 1
+
+    @data.push!(elem)
+
+    assert { expected == @data.size }
+  end
+
+  def test_pop!
+    expected = @data.size - 1
+
+    @data.pop!
+
+    assert { expected == @data.size }
+  end
+
+  def test_clear!
+    expected = []
+
+    @data.clear!
+
+    assert { expected == @data.to_a }
+  end
+
+  def test_empty?
+    assert { @data.empty?  }
+  end
+  # END
+end
+
+test_methods = StackTest.new({}).methods.select { |method| method.start_with? 'test_' }
+raise 'StackTest has not tests!' if test_methods.empty?
